411b130f419edba5fec48dc420bf04fbea29de47
zike <952218079@qq.com>
2021-05-31 12:50:31 +0800

    dd

--- 留言板.js
+++ 留言板.js
@@ -1,21 +1,19 @@
- document.addEventListener('DOMContentLoaded',function(){
-//1.获取元素
-var text = document.querySelector('#text');
-var btn = document.querySelector('button');
-var ul = document.querySelector('#liuyan');
-var appreant = ul.children;
+  //1.获取元素
+  var text = document.querySelector('#text');
+  var btn = document.querySelector('button');
+  var showtext=document.querySelector('.showtext');
+  var ultext=showtext.children[0];
+  var appreant = ultext.children;
 
-//2.注册事件
-btn.onclick = function () {
-    if (text.value == '') {
-        alert('请输入内容');
-        return false;
-    } else {
-        var li = document.createElement('li');
-        li.innerHTML = text.value;
-        ul.insertBefore(li, appreant[0]);
-        text.value = '';
-    }
-}
- })
- 
+  //2.注册事件
+  btn.onclick = function () {
+      if (text.value == '') {
+          alert('请输入内容');
+          return false;
+      } else {
+          var litext = document.createElement('li');
+          litext.innerHTML = text.value;
+          ultext.insertBefore(litext, appreant[0]);
+          text.value = '';
+      }
+  }
